1eb9d4928d2b95afdb6ff4e8ad22adae8ee6abd0
Bakeforfun <Baker@bakeforfun.local>
2016-11-01 23:20:47 +0300

    Initial commit

---
+++ Script.py
@@ -0,0 +1,14 @@
+# Chapter 1
+
+import nltk
+nltk.download()
+
+from nltk.book import *
+
+text1.concordance("monstrous")
+text1.similar("monstrous")
+text2.similar("monstrous")
+text2.common_contexts(["monstrous", "very"])
+text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
+text3.generate()  # not currently available
+
